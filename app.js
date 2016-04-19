e6adbeabfa998d40bb6679e149f0e58fbaa0ae4a
2betop <liaoxuezhi@icloud.com>
2016-04-19 17:59:31 +0800

    升级 yog-dev-tools

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json())
 // 注意这里面的.js，不是一般的.js 文件，而是相当于 express 的 route.
 app.use(require('yog-devtools')({
     view_path: '',    // 避免报错。
-    rewrite_file: path.join(DOCUMENT_ROOT, 'config', 'server.conf'),
+    rewrite_file: [path.join(DOCUMENT_ROOT, 'config', 'server.conf'), path.join(DOCUMENT_ROOT, 'mock', 'server.conf')],
     data_path: path.join(DOCUMENT_ROOT, 'test')
 }));
 
